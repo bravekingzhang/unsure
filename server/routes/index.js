b6c106cb995005bc296354b75bf8d8d5c0c4e84b
brzhang <1595819400@qq.com>
2019-05-10 15:15:52 +0800

    【新增】服务端添加好友。

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,16 +2,20 @@ var express = require('express');
 var router = express.Router();
 var db = require('../mongo/db')
 
-/* GET home page. */
+/* get方式拿数据 */
 router.get('/', function (request, response, next) {
     db.listFeeds(function (res) {
         console.log(res);
         response.json(res)
     });
-    // db.insertFeeds(function (res) {
-    //     console.log(res);
-    //     response.json(res)
-    // });
+
+});
+//put方式塞数据
+router.put('/', function (request, response, next) {
+    db.insertFeeds(function (res) {
+        console.log(res);
+        response.json(res)
+    });
 });
 
 module.exports = router;
